ba43fd5a864df3735a93999aa7cbb5da00f37dac
Sehyun Chung <sehyun.chung@icloud.com>
2023-04-06 16:33:51 +0900

    reduce overall performance impact?

--- content.js
+++ content.js
@@ -1,18 +1,34 @@
-function removeElonized() {
-  const loadingDoge = document.querySelector(
-    'div[aria-label*="Loading"]#placeholder > svg'
-  );
-  const logoDoge = document.querySelector(
-    'a[aria-label="Twitter"] > div > svg'
-  );
-
-  loadingDoge?.remove();
-  logoDoge?.remove();
+function removeElonized(loadingDogeSvg, logoDogeSvg) {
+  loadingDogeSvg?.remove();
+  logoDogeSvg?.remove();
 }
 
 const observer = new MutationObserver((mutationsList) => {
+  let loadingDogeSvg, logoDogeSvg;
+
   for (const mutation of mutationsList) {
-    removeElonized();
+    if (mutation.type === "childList" && mutation.addedNodes.length) {
+      if (!loadingDogeSvg) {
+        loadingDogeSvg = document.querySelector(
+          'div[aria-label*="Loading"]#placeholder > svg'
+        );
+      }
+
+      if (!logoDogeSvg) {
+        logoDogeSvg = document.querySelector(
+          'a[aria-label="Twitter"] > div > svg'
+        );
+      }
+
+      if (loadingDogeSvg || logoDogeSvg) {
+        removeElonized(loadingDogeSvg, logoDogeSvg);
+      }
+
+      // Disconnect the observer if both elements are removed
+      if (loadingDogeSvg && logoDogeSvg) {
+        observer.disconnect();
+      }
+    }
   }
 });
 
